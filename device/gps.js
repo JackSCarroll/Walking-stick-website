934078a0962e2f07d213610c737fd8b4e7e2a032
AnthonyP128 <anthony8121999@gmail.com>
2021-09-09 11:32:37 +1000

    Mqtt added

--- device/gps.js
+++ device/gps.js
@@ -1,9 +1,18 @@
 const SerialPort = require('serialport');
 const GPS = require('gps');
 const gps = new GPS;
+const mqtt = require('mqtt');
+const client = mqtt.connect("mqtt://broker.hivemq.com:1883");
 
 let latitude = 0;
 let longitude = 0;
+let latlong;
+let topic = "/gps/device/0";
+
+client.on('connect', () => {
+    console.log("MQTT Connected");
+    client.subscribe(topic);
+});
 
 const serialPort = new SerialPort('/dev/ttyACM0', { // change path
     baudRate: 9600,
@@ -15,8 +24,16 @@ const serialPort = new SerialPort('/dev/ttyACM0', { // change path
 gps.on('data', data => {
     latitude = gps.state.lat;
     longitude = gps.state.lon
-    console.log(latitude);
-    console.log(longitude);
+    latlong = [
+        {
+            "latitude": latitude
+        },
+        {
+            "longitude": longitude
+        }
+    ];
+    client.publish(topic, JSON.stringify(latlong));
+    console.log(topic, JSON.stringify(latlong));
 });
 
 serialPort.on('data', data => {
