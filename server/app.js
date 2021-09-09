40c2a7fb4d21351ea1f7d117d0cbf049a2517fb2
AnthonyP128 <anthony8121999@gmail.com>
2021-09-09 11:33:14 +1000

    mqtt added backend

--- server/app.js
+++ server/app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+const mqtt = require('mqtt');
+const client = mqtt.connect("mqtt://broker.hivemq.com:1883");
+
+let topic = "/gps/device/0";
 
 app.listen(3000, () => {
     console.log("Server listening on 3000");
 });
 
 app.use('/', express.static('../client'));
+
+client.on('connect', () => {
+    console.log("MQTT Connected");
+    client.subscribe(topic);
+});
+
+client.on('message', (topic, message) => {
+    console.log("Topic: " + topic);
+    console.log("Message: " + message);
+})
