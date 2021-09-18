2c7cbc8ec3f48030f4857233f4fa308130a09ae2
JoelP14 <80367345+JoelP14@users.noreply.github.com>
2021-09-18 14:07:56 +1000

    Update Button test.py
    Fixed to read coords with address

--- Sensors/Button test.py
+++ Sensors/Button test.py
@@ -3,30 +3,66 @@ import time
 import os
 import paho.mqtt.client as mqtt 
 import threading
+#filter these
+#import pandas as pd
+#import geopandas as gpd
+import geopy
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+
+#import matplotlib.pyplot as plt
+#import plotly_express as px
+#import tqdm
+#from tqdm._tqdm_notebook import tqdm_notebook
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 address = 'you are on x street'
+#"/gps/device/0"
+#"mqtt://broker.hivemq.com"
+global outputstring
+outputstring = ''
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("/gps/device/0")
 
-hostname = "mqtt://broker.hivemq.com"
-client = mqtt.Client(hostname, True, None, mqtt.MQTTv31)
-client.connect(hostname, port=1883, keepalive=60, bind_address="")
-client.subscribe("/gps/device/0")
+def on_message(client, userdata, msg):
+    global outputstring
+    print(msg.topic+" "+str(msg.payload))
+    outputstring = msg.payload
 
-def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
 
-client.on_message=on_message
-client.loop_forever()
+client.connect("broker.hivemq.com", 1883, 60)
 
+#client.loop_forever()
 
 while True: # Run forever
     #print("running")
     if GPIO.input(21) == False:
+        lat = ''
+        long = ''
         print("Button was pushed!")
+        client.loop_start()
+        time.sleep(1)
+        client.loop_stop()
+        outputstringTest = str(outputstring).split(',')
+            
+        lat = outputstringTest[0].split(':')[1]
+        long = outputstringTest[1].split(':')[1][:-2]
+        
+        locator = Nominatim(user_agent="myGeocoder")
+        coordinates = lat + ", " + long
+        
+        location = locator.reverse(coordinates)
+        location.raw
+        print(location.address)
+        address = location.address
+        
+        
+
         os.system("/home/pi/./speech.sh " + address)
         time.sleep(1)
